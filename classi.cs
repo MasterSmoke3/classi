f449b9caefeca7df7b0b4c94c88db7c845716f01
MasterSmoke3 <112854234+MasterSmoke3@users.noreply.github.com>
2023-11-02 22:28:41 +0500

    Add files via upload

---
+++ classi.cs
@@ -0,0 +1,25 @@
+class telephone
+{
+    string brend;
+    double price;
+
+    int memory;
+
+    string color;
+
+
+    public telephone(string brend, double price, int memory, string color)
+    {
+        this.brend = brend;
+        this.price = price;
+        this.memory = memory;
+        this.color = color;
+    }
+}
+
+class program {
+    public static void Main(string[] args){
+        
+    }
+}
+
